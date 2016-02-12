018f3b5c7e4c4be39a9e2af098cf818cc1a6eb89
asus4851 <hres.vadym@gmail.com>
2016-02-12 10:45:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="css/style.css"></script>
+</head>
+<body>
+
+	<div class="array_change_key_case">
+		<label class="label">array_change_key_case</label>
+	</div>
+</body>
+</html>
